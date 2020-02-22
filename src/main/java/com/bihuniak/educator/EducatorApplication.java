3cd1b5106f271dda291fc9d349300316ef01d02d
ghosty21 <ghostmaster528@gmail.com>
2020-02-22 14:16:42 +0100

    update

--- src/main/java/com/bihuniak/educator/EducatorApplication.java
+++ src/main/java/com/bihuniak/educator/EducatorApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class EducatorApplication {
@@ -20,7 +23,13 @@ public class EducatorApplication {
 
 	@PostConstruct
 	public void init() {
+
+
+
+
 		Car car = new Car("A4", "Audi", 4, 2.0, true);
-		myFirstRepository.save(car);
+		Car car2 = new Car("M3", "BMW", 4, 2.5, false);
+		Car car3 = new Car("Astra", "Opel", 4, 1.8, true);
+		myFirstRepository.saveAll(Arrays.asList(car, car2, car3));
 	}
 }
