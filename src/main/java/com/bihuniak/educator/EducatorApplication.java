4b3ebc2cb877c5af20f7eec98eb51ecf5e2602a1
ghosty21 <ghostmaster528@gmail.com>
2020-02-22 14:34:24 +0100

    update

--- src/main/java/com/bihuniak/educator/EducatorApplication.java
+++ src/main/java/com/bihuniak/educator/EducatorApplication.java
@@ -23,13 +23,11 @@ public class EducatorApplication {
 
 	@PostConstruct
 	public void init() {
-
-
-
-
-		Car car = new Car("A4", "Audi", 4, 2.0, true);
-		Car car2 = new Car("M3", "BMW", 4, 2.5, false);
-		Car car3 = new Car("Astra", "Opel", 4, 1.8, true);
-		myFirstRepository.saveAll(Arrays.asList(car, car2, car3));
+//		Car car = new Car("A4", "Audi", 4, 2.0d, true);
+//		Car car2 = new Car("M3", "BMW", 4, 2.5d, false);
+//		Car car3 = new Car("Astra", "Opel", 4, 1.8d, true);
+//		myFirstRepository.saveAll(Arrays.asList(car, car2, car3));
+        Car car - MyFirstRepository.findById(2L).get();
+        System.out.printIn(car);
 	}
 }
--- src/main/java/com/bihuniak/educator/basic/Car.java
+++ src/main/java/com/bihuniak/educator/basic/Car.java
@@ -18,12 +18,25 @@ public class Car {
     private double engineCapacity;
     private boolean secondHand;
 
+
     public Car(String model, String brand, int wheels, double engineCapacity, boolean secondHand) {
         this.model = model;
         this.brand = brand;
         this.wheels = wheels;
         this.engineCapacity = engineCapacity;
         this.secondHand = secondHand;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", wheels=" + wheels +
+                ", engineCapacity=" + engineCapacity +
+                ", secondHand=" + secondHand +
+                '}';
     }
 
     public Car() {
